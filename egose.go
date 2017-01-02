63d55ad281007810402cab3a84b85cdd8b64123e
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2017-01-02 14:34:36 +0900

    can display timelines

--- egose.go
+++ egose.go
@@ -54,14 +54,12 @@ func main() {
 
 	var query string
 	var count int
+	var tweets []twitter.Tweet
+	var search *twitter.Search
+
 	flag.StringVar(&query, "q", "", "Search query")
 	flag.IntVar(&count, "c", 10, "Search count")
 	flag.Parse()
-
-	if len(query) == 0 {
-		fmt.Println("Please specify search query.")
-		os.Exit(1)
-	}
 
 	oauthConfig := oauth1.NewConfig(config.TwitterConsumerKey, config.TwitterConsumerSecret)
 	token := oauth1.NewToken(config.TwitterAccessToken, config.TwitterAccessSecret)
@@ -69,19 +67,29 @@ func main() {
 
 	client := twitter.NewClient(httpClient)
 
-	search, _, err := client.Search.Tweets(&twitter.SearchTweetParams{
-		Query: query,
-		Count: count,
-	})
-
-	if err != nil {
-		fmt.Printf("Search API Error:%v\n", err)
-		os.Exit(1)
+	if len(query) == 0 {
+		tweets, _, err = client.Timelines.HomeTimeline(&twitter.HomeTimelineParams{
+			Count: count,
+		})
+		if err != nil {
+			fmt.Printf("twitter API Error:%v\n", err)
+			os.Exit(1)
+		}
+	} else {
+		search, _, err = client.Search.Tweets(&twitter.SearchTweetParams{
+			Query: query,
+			Count: count,
+		})
+		if err != nil {
+			fmt.Printf("twitter API Error:%v\n", err)
+			os.Exit(1)
+		}
+		tweets = search.Statuses
 	}
 
 	table := tablewriter.NewWriter(os.Stdout)
 	table.SetHeader([]string{"User", "Text", "URL"})
-	for _, tweet := range search.Statuses {
+	for _, tweet := range tweets {
 		table.Append([]string{tweet.User.Name, runewidth.Truncate(tweet.Text, 80, "..."), tweetURL(tweet)})
 	}
 	table.Render()
